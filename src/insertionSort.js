8f2a2632246b41d7febe784e4feeb7f5a7b4745f
github username <github email address>
2019-11-04 10:24:16 -0500

    Done.

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,11 @@
 function insertionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  for (let i = 1; i < arr.length; i++) {
+    let j = i - 1
+    let tmp = arr[i]
+    while (j >= 0 && arr[j] > tmp) {
+      arr[j + 1] = arr[j]
+      j--
+    }
+    arr[j+1] = tmp
+  }
 }
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,23 +1,4 @@
 function selectionSort(arr) {
-  // sorted = [];
-  // unsorted = arr.slice();
-
-  // for(let i = 0; i < arr.length; i++) {
-  //   let min = Infinity;
-  //   for(let j = 0; j < unsorted.length; j++) {
-  //     if (unsorted[j] < min) {
-  //       min = unsorted[j];
-  //     }
-  //   }
-
-  //   index = unsorted.findIndex(num => num === min);
-
-  //   sorted.push(min);
-  //   unsorted = [...unsorted.slice(0, index), ...unsorted.slice(index + 1)];
-  // }
-
-  // return sorted;
-
   for(let i = 0; i < arr.length; i++) {
     let min = arr[i];
     let index = null;
