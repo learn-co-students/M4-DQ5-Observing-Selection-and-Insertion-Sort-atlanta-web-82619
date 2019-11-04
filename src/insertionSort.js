f2a6b909c2457411c3a197aa19a5ceac358af8dd
Alexandria <alex.pugia816@hotmail.com>
2019-11-04 09:55:27 -0500

    done

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,10 @@
 function insertionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  let i = 0
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j > 0 && arr[j - 1] > arr[j]; j--){
+      let a = arr[j]
+      arr[j] = arr[j -1]
+      arr[j - 1] = a
+    }
+  }
 }
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,13 @@
 function selectionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  for (let i = 0; i < arr.length; i++) {
+    let jMin = i
+    for(let j = i; j < arr.length; j++) {
+      if (arr[j] < arr[jMin]) {
+        jMin = j
+      }
+    }
+    let a = arr[i]
+    arr[i] = arr[jMin]
+    arr[jMin] = a
+  }
 }
