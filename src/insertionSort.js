526d58a256c3bd249755c098499e55e11fa9f523
Andrea T <tostonar@gmail.com>
2019-11-04 10:17:20 -0500

    Done.

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,15 @@
 function insertionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  const length = arr.length;
+  for (let i = 0; i < length; i++) {
+    let element = arr[i];
+    let j;
+
+    for ( j = i - 1; j >= 0 && arr[j] > element ; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = element;
+  }
+  return arr;
 }
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,19 @@
 function selectionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  for (var i = 0; i < arr.length; i++) {
+
+    let min = i; //  storing the index of minimum element
+
+    for (var j = i + 1; j < arr.length; j++) {
+        if (arr[min] > arr[j]) {
+            min = j; // updating the index of minimum element
+        }
+    }
+
+    if (i !== min) {
+        [arr[ i ],arr[min]]= [arr[min],arr[ i ]];
+    }
+}
+return arr
 }
